3ea1b6205ef8b12355c9bf966b8951fa7bfe1e7a
sarthurdev <965089+sarthurdev@users.noreply.github.com>
2023-02-23 03:03:47 +0100

    hostap: T5003: Build Debian package with patch for AES-256-GCM

--- packages/hostap/build.sh
+++ packages/hostap/build.sh
@@ -2,29 +2,21 @@
 CWD=$(pwd)
 set -e
 
-SRC=hostap
-SRC_DEB=wpa
+SRC=wpa
 
 if [ ! -d ${SRC} ]; then
     echo "${SRC} directory does not exists, please 'git clone'"
     exit 1
 fi
-if [ ! -d ${SRC_DEB} ]; then
-    echo "${SRC_DEB} directory does not exists, please 'git clone'"
-    exit 1
-fi
 
 echo "I: Copy Debian build instructions"
-cp -a ${SRC_DEB}/debian ${SRC}
-# Preserve Debian's default of allowing TLSv1.0 for compatibility
-find ${SRC}/debian/patches -mindepth 1 ! -name allow-tlsv1.patch -delete
-echo 'allow-tlsv1.patch' > ${SRC}/debian/patches/series
+for f in patches/* ; do
+    cp $f ${SRC}/debian/patches/
+    echo $(basename $f) >> ${SRC}/debian/patches/series
+done
 
 # Build Debian package
 cd ${SRC}
-echo "I: Create new Debian Package version"
-version="$(git describe --tags | tr _ .)"
-dch -v ${version:7} "New version to support AES-GCM-256 for MACsec" -b
 
 echo "I: Build Debian hostap Package"
 dpkg-buildpackage -us -uc -tc -b -Ppkg.wpa.nogui
